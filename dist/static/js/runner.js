20f448c14d8070030d92eedeb961d458fba467d4
xiaolong Hao <2072451919@qq.com>
2019-07-13 18:17:57 +0800

    build

---
+++ dist/static/js/runner.js
@@ -0,0 +1,20 @@
+
+var consoleInfo = []
+// 重写window.error
+window.onerror = function (msg, url, row, col) {
+  consoleInfo.push({ data: { msg, url, row, col }, type: 'error' })
+  return false
+}
+// 重写console
+if (console) {
+  var ableMethods = ['log', 'info', 'debug', 'warn', 'error']
+  for (let item of ableMethods) {
+    console[item] = function (data) {
+      consoleInfo.push({ data, type: item })
+    }
+  }
+}
+// 重写document.write
+document.write = function (data) {
+  document.body.innerHTML += data
+}
