13c046eb6acb1728187a4a830c9eeffec8977d3d
xiaolong Hao <2072451919@qq.com>
2019-07-18 18:46:36 +0800

    Create compiler.js

---
+++ dist/static/js/compiler.js
@@ -0,0 +1,29 @@
+import marked from 'marked'
+
+function compileMarkDown(code) {
+  return marked(code, { sanitize: true })
+}
+
+function compileSass(code = '') {
+  const Sass = require('./sass')
+  Sass.setWorkerUrl('./sass.worker.js')
+  const sass = new Sass()
+  return new Promise((resolve, reject) => {
+    sass.compile(code, result => {
+      if (result.status === 0) resolve(result.text)
+      else reject(new Error('fail to get result'))
+    })
+  })
+}
+
+function compileTypeScript(code) {
+  let tyScript = process.env.TYPESCRIPT_CDN
+  console.log(tyScript)
+  let JSFile = new FileReader()
+}
+
+export {
+  compileMarkDown,
+  compileSass,
+  compileTypeScript
+}
