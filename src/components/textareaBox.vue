3fa9fca5fe4d66b6a331b6a94f61af397e853c84
xiaolong Hao <2072451919@qq.com>
2019-07-22 19:35:43 +0800

    change some function

--- src/components/textareaBox.vue
+++ src/components/textareaBox.vue
@@ -51,6 +51,7 @@ import Console from './console'
 import { codemirror } from 'vue-codemirror'
 import { mapState } from 'vuex'
 import * as compiler from '../../static/js/compiler.js'
+import judgeMode from '../utils/judgeMode.js'
 export default {
   props: {
     index: Number,
@@ -191,80 +192,6 @@ export default {
     }
   },
   methods: {
-    async judgeMode() {
-      const state = this.$store.state
-      const content = state.textBoxContent
-      const HTMLPrep = state.HTMLPrep
-      const CSSPrep = state.CSSPrep
-      const JSPrep = state.JSPrep
-      let HTMLCode = '',
-        CSSCode = '',
-        JSCode = ''
-
-      if (HTMLPrep === 'HTML') {
-        HTMLCode = content.HTML
-      } else if (HTMLPrep === 'MarkDown') {
-        await compiler
-          .compileMarkDown(content.HTML)
-          .then(code => {
-            HTMLCode = code
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      }
-
-      if (CSSPrep === 'CSS') {
-        CSSCode = content.CSS
-      } else if (CSSPrep === 'Sass' || CSSPrep === 'Scss') {
-        await compiler
-          .compileSass(content.CSS)
-          .then(code => {
-            CSSCode = code
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      } else if (CSSPrep === 'Less') {
-        await compiler
-          .compileLess(content.CSS)
-          .then(code => {
-            CSSCode = code.css
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      } else if (CSSPrep === 'Stylus') {
-        await compiler
-          .compileStylus(content.CSS)
-          .then(code => {
-            CSSCode = code
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      }
-
-      if (JSPrep === 'JavaScript') {
-        JSCode = content.JavaScript
-      } else if (JSPrep === 'TypeScript') {
-        JSCode = compiler.compileTypeScript(content.JavaScript)
-      } else if (JSPrep === 'CoffeeScript') {
-        await compiler
-          .compileCoffeeScript(content.JavaScript)
-          .then(code => {
-            JSCode = code
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      }
-      return {
-        HTMLCode,
-        CSSCode,
-        JSCode
-      }
-    },
     changeOptions(key, val) {
       this.$refs.editor.$options.parent.cmOptions[key] = val
     },
@@ -393,7 +320,7 @@ export default {
     async spliceHtml(time) {
       // get code
       let codeObj
-      await this.judgeMode().then(obj => {
+      await judgeMode(this.$store.state).then(obj => {
         codeObj = obj
       })
 
