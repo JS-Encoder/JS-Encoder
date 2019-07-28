85e186be76618aea7326f28baf1219a982c047e0
xiaolong Hao <2072451919@qq.com>
2019-07-28 11:14:36 +0800

    add some import files

--- src/utils/codeEditor.js
+++ src/utils/codeEditor.js
@@ -1,5 +1,36 @@
 /* eslint-disable */
 import CodeMirror from 'codemirror'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/mode/sass/sass'
+import 'codemirror/mode/stylus/stylus'
+import 'codemirror/mode/coffeescript/coffeescript'
+
+import 'codemirror/addon/fold/foldcode'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/fold/brace-fold'
+import 'codemirror/addon/fold/xml-fold'
+
+import 'codemirror/addon/hint/javascript-hint'
+import 'codemirror/addon/hint/html-hint'
+import 'codemirror/addon/hint/css-hint'
+import 'codemirror/addon/hint/show-hint'
+import 'codemirror/addon/hint/anyword-hint'
+
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/matchtags'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/edit/continuelist'
+
+import 'codemirror/addon/comment/comment'
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/addon/selection/active-line'
+import 'codemirror/addon/dialog/dialog'
+import 'codemirror/addon/display/fullscreen'
+import 'codemirror/keymap/sublime'
 
 export default function () {
   const cmOptions = {
@@ -25,7 +56,6 @@ export default function () {
     gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
     styleActiveLine: true
   }
-
   import('codemirror-emmet').then(emmet => {
     emmet(CodeMirror)
     cmOptions.extraKeys = {
