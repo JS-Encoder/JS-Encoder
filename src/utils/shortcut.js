92736be92482fd8a7e20e9cfd15424915988b793
xiaolong Hao <2072451919@qq.com>
2019-08-10 18:34:39 +0800

    Create shortcut.js

---
+++ src/utils/shortcut.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const shortcutList = [
+  {
+    keyboard: ['Tab'],
+    explain: 'Indent code'
+  }, {
+    keyboard: ['Shift', 'Alt', 'F'],
+    explain: 'Format code'
+  }, {
+    keyboard: ['Ctrl', 'Alt'],
+    explain: 'Turn on smart tips'
+  }, {
+    keyboard: ['Ctrl', 'Q'],
+    explain: 'Fold the code'
+  }, {
+    keyboard: ['Ctrl', '/'],
+    explain: 'Toggle comment on selected lines'
+  }, {
+    keyboard: ['Ctrl', 'Shift', 'D'],
+    explain: 'Duplicate line'
+  }, {
+    keyboard: ['Ctrl', 'D'],
+    explain: 'Select the current row'
+  }, {
+    keyboard: ['Shift', 'Ctrl', 'Up'],
+    explain: 'Swap line up'
+  }, {
+    keyboard: ['Shift', 'Ctrl', 'Down'],
+    explain: 'Swap line down'
+  }
+]
+
+export default shortcutList
