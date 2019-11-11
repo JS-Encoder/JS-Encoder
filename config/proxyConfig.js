293d27a79cf7fcf6230d8bce5c25c80afefdd2aa
xiaolong Hao <2072451919@qq.com>
2019-11-11 16:12:38 +0800

    deploy proxytable

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -1,11 +1,19 @@
 module.exports = {
   proxy: {
-    '/apis': {    //将www.exaple.com印射为/apis
+    '/github': {    //将www.exaple.com印射为/apis
       target: 'https://github.com',  // 接口域名
-      secure: false,  // 如果是https接口，需要配置这个参数
+      secure: true,  // 如果是https接口，需要配置这个参数
       changeOrigin: true,  //是否跨域
       pathRewrite: {
-        '^/apis': ''   //需要rewrite的,
+        '^/github': ''   //需要rewrite的,
+      }
+    },
+    '/gitUser': {
+      target: 'https://api.github.com',
+      secure: true,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/gitUser': ''
       }
     }
   }
