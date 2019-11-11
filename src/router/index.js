52aa13ec090618ff1fcd499597c2275bf7bc6c7b
xiaolong Hao <2072451919@qq.com>
2019-11-11 16:14:01 +0800

    deploy router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import editor from '@/components/editor.vue'
+import profile from '@/components/profile.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'HelloWorld',
-  //     component: HelloWorld
-  //   }
-  // ]
+  routes: [
+    {
+      path: '/',
+      redirect: 'editor',
+      component: editor,
+      children: [
+        {
+          path: 'editor',
+          component: editor
+        },
+        {
+          path: 'profile',
+          component: profile
+        }
+      ]
+    }
+  ],
+  mode: 'history'
 })
