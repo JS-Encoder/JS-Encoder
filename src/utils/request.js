84bc0c3666393bbaff558f397914212f2855159f
xiaolong Hao <2072451919@qq.com>
2019-11-11 16:14:38 +0800

    package axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,24 +1,26 @@
 /* eslint-disable */
 import axios from 'axios'
+import qs from 'qs'
+
 axios.defaults.withCredentials = true
 
 function post(url = '', params = {}) {
   return new Promise((resolve, reject) => {
-    axios.post(url, {
-      params
-    }).then(res => {
-      resolve(res.data)
-    }).catch(err => {
-      reject(err)
-    })
+    axios.post(url, qs.stringify(params))
+      .then(res => {
+        resolve(res.data)
+      }).catch(err => {
+        reject(err)
+      })
   })
 }
 
-function get(url = '', params = {}) {
+function get(url = '', config = {}) {
   return new Promise((resolve, reject) => {
-    axios.get(url, {
-      params
-    }).then(res => {
+    axios.get(
+      url,
+      config
+    ).then(res => {
       resolve(res.data)
     }).catch(err => {
       reject(err)
