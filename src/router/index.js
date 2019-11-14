fbec8b88fa70d504f62e3fcceec450e857ba1bbc
xiaolong Hao <2072451919@qq.com>
2019-11-14 12:57:59 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import editor from '@/components/editor.vue'
-import profile from '@/components/profile.vue'
-
+import Editor from '@/components/editor.vue'
+import Profile from '@/components/profile.vue'
+import store from '@/vuex/store'
 Vue.use(Router)
 
-export default new Router({
+/* eslint-disable*/
+const router = new Router({
   routes: [
     {
       path: '/',
-      redirect: 'editor',
-      component: editor,
-      children: [
-        {
-          path: 'editor',
-          component: editor
-        },
-        {
-          path: 'profile',
-          component: profile
-        }
-      ]
+      redirect: 'editor'
+    },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: Editor
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: {
+        judgeLogin: true
+      },
+      component: Profile
     }
   ],
-  mode: 'hash'
+  mode: 'hash',
+  base: '/'
 })
+
+function loadHistorySession() {
+  const jsEcdStore = sessionStorage.getItem('jsEcdStore')
+  if (jsEcdStore !== null) {
+    store.replaceState(JSON.parse(jsEcdStore))
+  }
+}
+// 获取sessionStorage
+loadHistorySession()
+
+// 路由守卫
+router.beforeEach(function (to, from, next) {
+  // 判断此路由是否需要拦截
+  if (!to.meta.judgeLogin) return next()
+  if (store.state.loginStatus) return next()
+  alert('please login')
+  router.push({
+    name: 'editor'
+  })
+})
+
+export default router
