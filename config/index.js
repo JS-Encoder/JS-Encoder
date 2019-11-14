e885c9801a12d3250cacca751215fe543808b03c
xiaolong Hao <2072451919@qq.com>
2019-11-14 12:56:30 +0800

    change port

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'https://longgererer.github.io/', // can be overwritten by process.env.HOST
-    port: 4040, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
