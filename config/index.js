46dc9bbb059b38c45a810015fbda772c4337637b
xiaolong Hao <2072451919@qq.com>
2020-05-19 10:49:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -19,8 +19,6 @@ module.exports = {
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-
-    
     /**
      * Source Maps
      */
