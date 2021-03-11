187fce4a646dd825332bbbacf80d22243b3dee2a
xiaolong Hao <2072451919@qq.com>
2021-03-11 20:33:27 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,14 @@ const resolve = (dir) => path.join(__dirname, dir)
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
 
 module.exports = {
-  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : './', // 默认'/'，部署应用包时的基本 URL
-  // outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
-  // assetsDir: "", // 相对于outputDir的静态资源(js、css、img、fonts)目录
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './', // 默认'/'，部署应用包时的基本 URL
+  outputDir: 'dist', // 'dist', 生产环境构建文件的目录
+  assetsDir: "static", // 相对于outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false,
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
   productionSourceMap: !IS_PROD, // 生产环境的 source map
   parallel: require('os').cpus().length > 1,
   pwa: {},
-  publicPath: '/',
   devServer: {
     proxy: {
       '/cdnJS': {
