065f13e6cf3570cd85bfbddc73ea890c9f915a99
xiaolong Hao <2072451919@qq.com>
2021-08-13 16:48:19 +0800

    修改生产环境打包配置减少包的大小

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+const cdn = {
+  externals: {
+    vue: 'Vue',
+    vuex: 'Vuex',
+    'axios': 'axios'
+  },
+  js: [
+    'https://cdn.bootcdn.net/ajax/libs/vue/2.6.11/vue.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/vuex/3.5.1/vuex.min.js',
+    'https://cdn.bootcdn.net/ajax/libs/axios/0.19.2/axios.min.js'
+  ]
+}
+// 本地环境是否需要使用cdn
+const devNeedCdn = false
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'development' ? '/' : './', // 默认'/'，部署应用包时的基本 URL
@@ -38,6 +54,23 @@ module.exports = {
       include: /node_modules/,
       type: 'javascript/auto'
     })
+    // 配置gzip
+    //gzip压缩
+    const productionGzipExtensions = ['html', 'js', 'css']
+    config.plugins.push(
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp(
+          '\\.(' + productionGzipExtensions.join('|') + ')$'
+        ),
+        threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+        deleteOriginalAssets: false // 删除原文件
+      })
+    )
+
+    if (IS_PROD || devNeedCdn) config.externals = cdn.externals
   },
   chainWebpack: (config) => {
     config.resolve.alias
@@ -48,6 +81,14 @@ module.exports = {
       .set('@views', resolve('src/views'))
       .set('@store', resolve('src/store'))
       .set('@utils', resolve('src/utils'))
+    config.plugin('html').tap(args => {
+      // 生产环境或本地需要cdn时，才注入cdn
+      if (IS_PROD || devNeedCdn) args[0].cdn = cdn
+      return args
+    })
+    config
+      .plugin('webpack-bundle-analyzer')
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
   },
   css: {
     extract: IS_PROD,
