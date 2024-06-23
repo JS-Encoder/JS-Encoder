0880e857ca9b23ce833caad0ec85dd5289ac1e4c
Longgererer <2072451919@qq.com>
2024-06-23 17:07:49 +0800

    update: 调整vue组件模板缩进

--- src/utils/config/template/code/vue-component/code.vue
+++ src/utils/config/template/code/vue-component/code.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        message: "This is a template of Vue3 Component!",
-      }
-    },
-  }
+export default {
+  data() {
+    return {
+      message: "This is a template of Vue3 Component!",
+    }
+  },
+}
 </script>
 
 <style scoped></style>
